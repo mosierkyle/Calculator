7fd593a82004dfd01c895b9eae04d3fdf3ae6015
Kyle Mosier <mosierkyle@gmail.com>
2023-08-16 10:14:25 -0700

    Create function for when a number is click
    Created a function that will be called by an event when a specific number is clicked. The fucntion also displays the numbers.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 'use strict'
+let num1;
+let num2;
+let operator;
+let resultText = document.querySelector('#text')
 
 function addition(num1, num2) {
     return (num1) + (num2);
@@ -22,4 +26,28 @@ function sign(num) {
 
 function percent(num) {
     return num / 100
+}
+
+function clickNumber(num) {
+  let number = String(num);
+  if(num1 === undefined) {
+    num1 = number;
+    resultText.textContent = num1;
+    return
+  }
+  if(operator === undefined) {
+    num1 += number;
+    resultText.textContent = num1;
+    return
+  }
+  if(operator !== undefined && num1 !== undefined && num2 === undefined) {
+    num2 = number;
+    resultText.textContent = num2
+    return
+  }
+  if(operator !== undefined && num1 !== undefined && num2 !== undefined) {
+    num2 += number;
+    resultText.textContent = num2
+    return
+  }
 }
