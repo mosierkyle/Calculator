21aa39fcba31eaf0befc87ca326ff8aa9fb3648a
Kyle Mosier <mosierkyle@gmail.com>
2023-08-15 21:00:39 -0700

    Create basic button functions for calc
    Created all of the operator function for the calculator

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 'use strict'
+
+function addition(num1, num2) {
+    return (num1) + (num2);
+  };
+  
+function subtraction(num1, num2) {
+    return (num1) - (num2);
+  };
+  
+function multiplication(num1, num2) {
+    return (num1) * (num2);
+  };
+  
+function division(num1, num2) {
+    return (num1) / (num2);
+  }
+
+function sign(num) {
+    return num * (-1);
+}
+
+function percent(num) {
+    return num / 100
+}
